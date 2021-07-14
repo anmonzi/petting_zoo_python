0f3f3d2e49005439bd0c73f7c23cc991bca00d32
Ashton Monzi <anmonzi@gmail.com>
2021-07-14 13:26:33 -0500

    Refactored walking animals to have a shift property

--- petting_area/petting_area_animals.py
+++ petting_area/petting_area_animals.py
@@ -7,60 +7,65 @@ from datetime import date
 
 class Llama:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         # Establish the propertis of each animal with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
-miss_fuzz = Llama("Miss Fuzz", "domestic llama")
+miss_fuzz = Llama("Miss Fuzz", "domestic llama", "morning")
 print(miss_fuzz.name)
 
 class Goat:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         # Establish the propertis of each animal with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
-lady_goat = Goat("The G.O.A.T", "domestic goat")
+lady_goat = Goat("The G.O.A.T", "domestic goat", "morning")
 print(lady_goat.name)
 
 class Donkey:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         # Establish the propertis of each animal with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
-donkey = Donkey("Donkey", "domestic donkey")
+donkey = Donkey("Donkey", "domestic donkey", "midday")
 print(donkey.name)
 
 class Pony:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         # Establish the propertis of each animal with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
-little_pony = Pony("Princess", "domestic pony")
+little_pony = Pony("Princess", "domestic pony", "afternoon")
 print(little_pony.name)
 
 class Alpaca:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         # Establish the propertis of each animal with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
-silver_bullet = Alpaca("Silver Bullet", "domestic alpaca")
-print(silver_bullet.name)
+silver_bullet = Alpaca("Silver Bullet", "domestic alpaca", "afternoon")
+print(f"{silver_bullet.name} the {silver_bullet.species} is available to pet during the {silver_bullet.shift} shift.")

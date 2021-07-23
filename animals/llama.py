f6c77af3407d0db38c5b784190d784041ec605c8
Ashton Monzi <anmonzi@gmail.com>
2021-07-23 16:44:05 -0500

    Ch.10 Done - Multiple inheritance added along with added behaviors to attraction parent class

--- animals/llama.py
+++ animals/llama.py
@@ -1,14 +1,27 @@
+# The package syntax is what allows for these clean import statements
 from animals import Animal
 from datetime import date
+from movements import Walking
 
 # Designate Llama as a child class by adding (Animal) after the class name
-class Llama(Animal):
+class Llama(Animal, Walking):
     # Remove redundant properties from Llama's initialization, and set their values via Animal
     def __init__(self, name, species, shift, food, chip_number):
-        super().__init__(name, species, food, chip_number)
+        Animal.__init__(self, name, species, food, chip_number)
+        Walking.__init__(self)
         # Establish the different propertis of each animal with a default value
-        self.walking = True
         self.shift = shift  # stays on because not all animals have shifts
 
     def feed(self):
         print(f'on {date.today()}, {self.name} had "Rockytop" sung to it so it would eat its {self.food}')
+
+    def __str__(self):
+        return f'{self.name} the Llama'
+
+
+
+
+# NOTE: When a class inherits from two parents you have to discard using 
+# the super().__init__() syntax and explicitly invoke 
+# the initialization method of both by name. You also need to pass self as an argument
+# - something that is not needed when you use the super() abstraction.
--- animals/mallard.py
+++ animals/mallard.py
@@ -1,10 +1,25 @@
 from animals import Animal
+from movements import Walking, Swimming
 
 # Designate Goat as a child class by adding (Animal) after the class name
-class Mallard(Animal):
+class Mallard(Animal, Walking, Swimming):
     # Remove redundant properties from Goat's initialization, and set their values via Animal
     def __init__(self, name, species, food, chip_number):
-        super().__init__(name, species, food, chip_number)
+        Animal.__init__(self, name, species, food, chip_number)
+        Walking.__init__(self)
+        Swimming.__init__(self)
         # Establish the propertis of each animal with a default value
-        self.swimming = True
         self.venomous = False
+
+    def quack(self):
+        print("The mallard quacks! A lot!")
+
+    # run is defined in the Walking parent class, but also here. This run method will take precedence and Walking's run method will not be called by Goose instances
+    def run(self):
+        print(f'{self} waddles')
+
+    def swim(self):
+        print(f'{self} uses his webbed feet to swim.')
+
+    def __str__(self):
+        return f'{self.name} the mallard duck'
